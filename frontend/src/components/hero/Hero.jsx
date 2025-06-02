127d310744decbcd15e37362b8132adf77d65ae3
Radwan Mansur <raydiaz1899@gmail.com>
2025-06-02 22:29:51 +0300

    day 1

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -3,8 +3,16 @@ import './hero.css'
 function Hero() {
     return (
         <div className="contaienr hero">
-            <div className='content'>
+            <div className='content c1' id='home'>
                 <h1>welcome to my practice app</h1>
+                <p>good things happen to those who never give up.</p>
+            </div>
+            <div className='content c2' id='form'>
+
+                <p>good things happen to those who never give up.</p>
+            </div>
+            <div className='content c3' id='result'>
+
                 <p>good things happen to those who never give up.</p>
             </div>
 
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -3,11 +3,11 @@ import './nav.css';
 function Nav() {
     return (
 
-        <nav className='nav container'>
+        <nav className='nav '>
             <ul>
-                <li>home</li>
-                <li>about</li>
-                <li>contact</li>
+                <li><a href="#home">home</a></li>
+                <li><a href="#form">form</a></li>
+                <li><a href="#result">result</a></li>
             </ul>
         </nav>
     )
