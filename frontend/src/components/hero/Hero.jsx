c3bb2a8c4ab3cf68af96fc38ead222cf5d146e7f
Radwan Mansur <raydiaz1899@gmail.com>
2025-06-02 20:59:53 +0300

    first changes

---
+++ frontend/src/components/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import './hero.css'
+
+function Hero() {
+    return (
+        <div className="contaienr hero">
+            <div className='content'>
+                <h1>welcome to my practice app</h1>
+                <p>good things happen to those who never give up.</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
---
+++ frontend/src/components/nav/Nav.jsx
@@ -0,0 +1,16 @@
+import './nav.css';
+
+function Nav() {
+    return (
+
+        <nav className='nav container'>
+            <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
