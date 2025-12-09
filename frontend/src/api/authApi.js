5c5bbb5c7314908ca1035286ab17df366b136720
Radwan Mansur <raydiaz1899@gmail.com>
2025-12-09 03:22:06 +0200

    redux setup

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,3 +3,7 @@ import myAxios from './myAxios'
 export function registerUser(userData) {
   return myAxios.post('/auth/register', userData)
 }
+
+export function loginUser(credentials) {
+  return myAxios.post('/auth/login', credentials)
+}
